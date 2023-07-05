51751703caf4905b248433c95a7ef83d91d3eebc
Shubham Urkade <surkade@vmware.com>
2023-07-05 05:38:56 +0000

    Enable get_running_head_node function in commands.py

--- python/ray/autoscaler/_private/commands.py
+++ python/ray/autoscaler/_private/commands.py
@@ -1371,7 +1371,6 @@ def _get_running_head_node(
     }
     nodes = provider.non_terminated_nodes(head_node_tags)
 
-    return nodes[0]
     head_node = None
     _backup_head_node = None
     for node in nodes:
